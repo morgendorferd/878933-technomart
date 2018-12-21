df8a9f9b639b89b6c7ff16275e204a7e854ea6cf
morgendorferd <d-hohlova94@mail.ru>
2018-12-22 01:02:31 +0300

    Задание 8
    Добавила попап в каталог, подогнала шапку под пп, стилизовала состояния кнопок на карточке товара в каталоге

---
+++ js/catalog.js
@@ -0,0 +1,22 @@
+var cartLink = document.querySelector(".cart-link"),
+    cartPopup = document.querySelector(".modal-cart"),
+    cartClose = document.querySelector('.modal-cart-close')
+
+ cartLink.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   cartPopup.classList.add("modal-show");
+});
+
+  cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+   if (evt.keyCode === 27) {
+     evt.preventDefault();
+     if (cartPopup.classList.contains("modal-show")) {
+       cartPopup.classList.remove("modal-show");
+     }
+   }
+ });
---
+++ js/main.js
@@ -0,0 +1,83 @@
+var contactsLink = document.querySelector(".main-contacts-link"),
+    contactsPopup = document.querySelector(".modal-write-us"),
+    contactsBtnClose = contactsPopup.querySelector(".modal-write-us-close"),
+    contactsForm = contactsPopup.querySelector("form"),
+    contactsName = contactsPopup.querySelector("input[name=Имя]"),
+    contactsEmail =contactsPopup.querySelector("input[name=Почта]"),
+    contactsTextarea =contactsPopup.querySelector("textarea[name=Комментарий]"),
+    mapLink = document.querySelector(".contacts-map"),
+    mapPopup = document.querySelector(".modal-map"),
+    mapClose = document.querySelector(".modal-map-close"),
+    cartLink = document.querySelector(".cart-link"),
+    cartPopup = document.querySelector(".modal-cart");
+
+
+var isStorageSupport = true;
+var storage = "";
+
+ try {
+   storage = localStorage.getItem("name");
+ } catch (err) {
+   isStorageSupport = false;
+ }
+
+contactsLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+ contactsPopup.classList.add("modal-show");
+
+  if (storage) {
+    contactsName.value = storage;
+    contactsEmail.focus();
+  } else {
+    contactsName.focus();
+}
+});
+
+contactsBtnClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+ contactsPopup.classList.remove("modal-show");
+});
+
+contactsForm.addEventListener("submit", function(evt) {
+  if (!contactsName.value || !contactsEmail.value || !contactsTextarea.value) {
+    evt.preventDefault();
+    console.log ("Заполните формы");
+  } else {
+    if (isStorageSupport) {
+    localStorage.setItem("name", contactsName.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+   if (evt.keyCode === 27) {
+     evt.preventDefault();
+     if (contactsPopup.classList.contains("modal-show")) {
+      contactsPopup.classList.remove("modal-show");
+     }
+   }
+ });
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+   if (evt.keyCode === 27) {
+     evt.preventDefault();
+     if (mapPopup.classList.contains("modal-show")) {
+       mapPopup.classList.remove("modal-show");
+     }
+   }
+ });
+
+ cartLink.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   cartPopup.classList.add("modal-show");
+});
