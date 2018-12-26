eea4072a6a2dd9ad7a395044068b6816bc8533c8
morgendorferd <d-hohlova94@mail.ru>
2018-12-26 14:25:45 +0300

    Слайдер на js

--- js/main.js
+++ js/main.js
@@ -9,8 +9,70 @@ mapLink = document.querySelector(".contacts-map"),
 mapPopup = document.querySelector(".modal-map"),
 mapClose = document.querySelector(".modal-map-close"),
 cartLink = document.querySelector(".cart-link"),
-cartPopup = document.querySelector(".modal-cart");
+cartPopup = document.querySelector(".modal-cart"),
+btnPrev = document.querySelector(".promo-slider-toggle--prev"),
+btnNext = document.querySelector("promo-slider-toggle--next"),
+images = document.querySelector("promo-slide"),
+pointsArray = document.querySelectorAll("promo-slider-control");
+var counter = 0;
+btnPrev.disabled = true;
 
+btnPrev.addEventListener('click', function() {
+    images[counter].classList.remove('shown');
+    pointsArray[counter].classList.remove('promo-slider-control--active');
+    counter--;
+    images[counter].classList.add('shown');
+    pointsArray[counter].classList.add('promo-slider-control--active');
+    btnNext.disabled = false;
+
+    if (counter === 0) {
+      btnPrev.disabled = true;
+    }
+});
+
+btnNext.addEventListener('click', function() {
+    images[counter].classList.remove('shown');
+    pointsArray[counter].classList.remove('promo-slider-control--active');
+    counter++;
+    images[counter].classList.add('shown');
+    pointsArray[counter].classList.add('promo-slider-control--active');
+    console.log(btnPrev.disabled);
+    btnPrev.disabled = false;
+
+    if (counter === images.length - 1) {
+      btnNext.disabled = true;
+    }
+});
+
+let slideThroughPoints = function () {
+  [].forEach.call(pointsArray, function(point, index) {
+    point.addEventListener('click', function (evt) {
+      if (index === counter) {
+          evt.preventDefault();
+          return;
+      } else {
+        pointsArray[counter].classList.remove('promo-slider-control--active');
+          images[counter].classList.remove('shown');
+          counter = index;
+          images[index].classList.add('shown');
+          point.classList.add('promo-slider-control--active');
+
+          if (counter === images.length - 1) {
+            btnNext.disabled = true;
+            btnPrev.disabled = false;
+          } else if (counter === 0) {
+            btnNext.disabled = false;
+            btnPrev.disabled = true;
+          } else {
+            btnNext.disabled = false;
+            btnPrev.disabled = false;
+          }
+      }
+    });
+  });
+};
+
+slideThroughPoints();
 
 var isStorageSupport = true;
 var storage = "";
