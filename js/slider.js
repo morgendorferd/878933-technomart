c37fc3a4cfbbe30e5f7bf7717f220bc672e9bd59
morgendorferd <d-hohlova94@mail.ru>
2018-12-26 17:00:40 +0300

    Добавила недостающие элементы

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,16 @@
 btnPrev = document.querySelector(".promo-slider-toggle--prev"),
 btnNext = document.querySelector(".promo-slider-toggle--next"),
-images = document.querySelector(".promo-slide"),
+images = document.querySelectorAll(".promo-slide"),
 pointsArray = document.querySelectorAll(".promo-slider-control");
 var counter = 0;
 btnPrev.disabled = true;
 
-btnPrev.addEventListener('click', function() {
-    images[counter].classList.remove('shown');
-    pointsArray[counter].classList.remove('promo-slider-control--active');
+btnPrev.addEventListener("click", function() {
+    images[counter].classList.remove("shown");
+    pointsArray[counter].classList.remove("promo-slider-control--active");
     counter--;
-    images[counter].classList.add('shown');
-    pointsArray[counter].classList.add('promo-slider-control--active');
+    images[counter].classList.add("shown");
+    pointsArray[counter].classList.add("promo-slider-control--active");
     btnNext.disabled = false;
 
     if (counter === 0) {
@@ -18,12 +18,12 @@ btnPrev.addEventListener('click', function() {
     }
 });
 
-btnNext.addEventListener('click', function() {
-    images[counter].classList.remove('shown');
-    pointsArray[counter].classList.remove('promo-slider-control--active');
+btnNext.addEventListener("click", function() {
+    images[counter].classList.remove("shown");
+    pointsArray[counter].classList.remove("promo-slider-control--active");
     counter++;
-    images[counter].classList.add('shown');
-    pointsArray[counter].classList.add('promo-slider-control--active');
+    images[counter].classList.add("shown");
+    pointsArray[counter].classList.add("promo-slider-control--active");
     console.log(btnPrev.disabled);
     btnPrev.disabled = false;
 
@@ -34,16 +34,16 @@ btnNext.addEventListener('click', function() {
 
 let slideThroughPoints = function () {
   [].forEach.call(pointsArray, function(point, index) {
-    point.addEventListener('click', function (evt) {
+    point.addEventListener("click", function (evt) {
       if (index === counter) {
           evt.preventDefault();
           return;
       } else {
-        pointsArray[counter].classList.remove('promo-slider-control--active');
-          images[counter].classList.remove('shown');
+        pointsArray[counter].classList.remove("promo-slider-control--active");
+          images[counter].classList.remove("shown");
           counter = index;
-          images[index].classList.add('shown');
-          point.classList.add('promo-slider-control--active');
+          images[index].classList.add("shown");
+          point.classList.add("promo-slider-control--active");
 
           if (counter === images.length - 1) {
             btnNext.disabled = true;
