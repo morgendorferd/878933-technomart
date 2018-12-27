8e2cc7a69819430ba8f670f6094b67e5d3351452
morgendorferd <d-hohlova94@mail.ru>
2018-12-27 23:03:57 +0300

    ошибки

--- js/slider.js
+++ js/slider.js
@@ -14,8 +14,6 @@ var pointsArray = document.querySelectorAll('.promo-slider-control');
 
 var counter2 = images.indexOf(shownImage);
 
-var counter = 0;
-
 btnPrev.addEventListener('click', function() {
 
     if (counter2 === 0) {
@@ -47,19 +45,6 @@ btnNext.addEventListener('click', function() {
     images[counter2].classList.add('shown');
     pointsArray[counter2].classList.add('promo-slider-control--active');
 });
-
-
-
-// Another 4 ways to make it work in IE
-
-// 1) const naturalPointsArr = Array.from(pointsArray);
-// 2) const naturalPointsArr = Array.prototype.slice.call(pointsArray);
-// он же - const naturalPointsArr = [].slice.call(document.querySelectorAll('.promo__slider-control'), 0);
-// 3) const naturalPointsArr = [...document.querySelectorAll('.promo__slider-control')];
-// 4) using standart cycle "for";
-
-
-
 
 let slideThroughPoints = function () {
   [].forEach.call(pointsArray, function(point, index) {
