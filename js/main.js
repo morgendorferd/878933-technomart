a1f6ff7d91381600e371cac8970ed745773553dd
morgendorferd <d-hohlova94@mail.ru>
2019-01-08 14:46:36 +0300

    заменила counter2 на counter

--- js/main.js
+++ js/main.js
@@ -102,50 +102,50 @@ var images = Array.prototype.slice.call(document.querySelectorAll(".promo-slide"
 var shownImage = document.querySelector(".shown");
 var shownPoint = document.querySelector(".promo-slider-control--active");
 var pointsArray = document.querySelectorAll(".promo-slider-control");
-var counter2 = images.indexOf(shownImage);
+var counter = images.indexOf(shownImage);
 
 btnPrev.addEventListener("click", function() {
 
-    if (counter2 === 0) {
-      images[counter2].classList.remove("shown");
-      pointsArray[counter2].classList.remove("promo-slider-control--active");
-      counter2 = images.length - 1;
+    if (counter === 0) {
+      images[counter].classList.remove("shown");
+      pointsArray[counter].classList.remove("promo-slider-control--active");
+      counter = images.length - 1;
     } else {
-      images[counter2].classList.remove("shown");
-      pointsArray[counter2].classList.remove("promo-slider-control--active");
-      counter2--;
+      images[counter].classList.remove("shown");
+      pointsArray[counter].classList.remove("promo-slider-control--active");
+      counter--;
     }
 
-    images[counter2].classList.add("shown");
-    pointsArray[counter2].classList.add("promo-slider-control--active");
+    images[counter].classList.add("shown");
+    pointsArray[counter].classList.add("promo-slider-control--active");
 });
 
 btnNext.addEventListener("click", function() {
 
-    if (counter2 === images.length - 1) {
-        images[counter2].classList.remove("shown");
-        pointsArray[counter2].classList.remove("promo-slider-control--active");
-        counter2 = 0;
+    if (counter === images.length - 1) {
+        images[counter].classList.remove("shown");
+        pointsArray[counter].classList.remove("promo-slider-control--active");
+        counter = 0;
     } else {
-      images[counter2].classList.remove("shown");
-      pointsArray[counter2].classList.remove("promo-slider-control--active");
-      counter2++;
+      images[counter].classList.remove("shown");
+      pointsArray[counter].classList.remove("promo-slider-control--active");
+      counter++;
     }
 
-    images[counter2].classList.add("shown");
-    pointsArray[counter2].classList.add("promo-slider-control--active");
+    images[counter].classList.add("shown");
+    pointsArray[counter].classList.add("promo-slider-control--active");
 });
 
 let slideThroughPoints = function () {
   [].forEach.call(pointsArray, function(point, index) {
     point.addEventListener("click", function (evt) {
-      if (index === counter2) {
+      if (index === counter) {
           evt.preventDefault();
           return;
       } else {
-          pointsArray[counter2].classList.remove("promo-slider-control--active");
-          images[counter2].classList.remove("shown");
-          counter2 = index;
+          pointsArray[counter].classList.remove("promo-slider-control--active");
+          images[counter].classList.remove("shown");
+          counter = index;
           images[index].classList.add("shown");
           point.classList.add("promo-slider-control--active");
             btnNext.disabled = false;
