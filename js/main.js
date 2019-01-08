1e717cee42e591902e17f5e59c209e611a14c772
morgendorferd <d-hohlova94@mail.ru>
2019-01-08 14:38:53 +0300

    Проверка переменных в js

--- js/main.js
+++ js/main.js
@@ -1,92 +1,99 @@
+// index.html
+// переменные, связанные с формой обращения
 var contactsLink = document.querySelector(".main-contacts-link"),
 contactsPopup = document.querySelector(".modal-write-us"),
 contactsBtnClose = contactsPopup.querySelector(".modal-write-us-close"),
 contactsForm = contactsPopup.querySelector("form"),
 contactsName = contactsPopup.querySelector("input[name=Имя]"),
 contactsEmail =contactsPopup.querySelector("input[name=Почта]"),
-contactsTextarea =contactsPopup.querySelector("textarea[name=Комментарий]"),
-mapLink = document.querySelector(".contacts-map"),
+contactsTextarea =contactsPopup.querySelector("textarea[name=Комментарий]");
+
+// переменные, связанные с картой
+var mapLink = document.querySelector(".contacts-map"),
 mapPopup = document.querySelector(".modal-map"),
-mapClose = document.querySelector(".modal-map-close");
+mapClose = mapPopup.querySelector(".modal-map-close");
 
 // переменные, используемые в catalog.html
 var cartPopup = document.querySelector(".modal-cart"),
 cartClose = document.querySelector(".modal-cart-close"),
 cartLinkElements = document.querySelectorAll(".cart-link");
 
-var isStorageSupport = true;
-var storage = "";
+// попап с формой обращения
+if (contactsLink, contactsPopup, contactsBtnClose, contactsForm, contactsName, contactsEmail, contactsTextarea) {
+  var isStorageSupport = true;
+  var storage = "";
 
-try {
-  storage = localStorage.getItem("name");
-} catch (err) {
-  isStorageSupport = false;
-}
+  try {
+    storage = localStorage.getItem("name");
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
-contactsLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  contactsPopup.classList.add("modal-show");
-
-  if (storage) {
-    contactsName.value = storage;
-    contactsEmail.focus();
-  } else {
-    contactsName.focus();
-  };
-
-  if (contactsPopup == "undefined") {
-    console.log ("Undefined")
-}
-});
-
-contactsBtnClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  contactsPopup.classList.remove("modal-show");
-  contactsPopup.classList.remove("modal-error");
-});
-
-contactsForm.addEventListener("submit", function(evt) {
-  if (!contactsName.value || !contactsEmail.value || !contactsTextarea.value) {
+  contactsLink.addEventListener("click", function (evt) {
     evt.preventDefault();
+    contactsPopup.classList.add("modal-show");
+
+    if (storage) {
+      contactsName.value = storage;
+      contactsEmail.focus();
+    } else {
+      contactsName.focus();
+    }
+  });
+
+  contactsBtnClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contactsPopup.classList.remove("modal-show");
     contactsPopup.classList.remove("modal-error");
-    contactsPopup.offsetWidth = contactsPopup.offsetWidth;
-    contactsPopup.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("name", contactsName.value);
-    }
-  }
-});
+  });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (contactsPopup.classList.contains("modal-show")) {
-      contactsPopup.classList.remove("modal-show");
+  contactsForm.addEventListener("submit", function(evt) {
+    if (!contactsName.value || !contactsEmail.value || !contactsTextarea.value) {
+      evt.preventDefault();
       contactsPopup.classList.remove("modal-error");
+      contactsPopup.offsetWidth = contactsPopup.offsetWidth;
+      contactsPopup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("name", contactsName.value);
+      }
     }
-  }
-});
+  });
 
-mapLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (contactsPopup.classList.contains("modal-show")) {
+        contactsPopup.classList.remove("modal-show");
+        contactsPopup.classList.remove("modal-error");
+      }
+    }
+  });
+}
 
-mapClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+// попап с картой
+if (mapLink, mapPopup, mapClose) {
+  mapLink.addEventListener("click", function (evt) {
     evt.preventDefault();
-    if (mapPopup.classList.contains("modal-show")) {
-      mapPopup.classList.remove("modal-show");
-    }
-  }
-});
+    mapPopup.classList.add("modal-show");
+  });
 
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
+}
+
+// слайдер
 (function () {
 
 var btnPrev = document.querySelector(".promo-slider-toggle--prev"),
